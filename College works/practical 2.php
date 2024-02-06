5cee08745b6bae7182caaa87294298317f761327
rishabh2-49 <109406744+rishabh2-49@users.noreply.github.com>
2024-02-06 22:31:46 +0530

    Add files via upload

---
+++ College works/practical 2.php
@@ -0,0 +1,24 @@
+<?php
+$a=69;
+$b=45;
+
+$c=$a+$b;
+echo "Sum of $a and $b is $c </br>";
+
+
+$c=$a-$b;
+echo "sub of $a and $b is : $c</br>";
+
+
+$c=$a*$b;
+echo "Multiplication of $a and $b is : $c</br>";
+
+
+$c=$a/$b;
+echo "division of $a and $b is : $c</br>";
+
+$c=$a%$b;
+echo "modulo of $a and $b is : $c</br>";
+
+
+?>
