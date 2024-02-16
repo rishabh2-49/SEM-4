64e82d1c894f1044570248ffa693e8b94f744295
rishabh2-49 <109406744+rishabh2-49@users.noreply.github.com>
2024-02-16 03:47:30 -0800

    Update practical 5.1.php

--- College works/practical 5.1.php
+++ College works/practical 5.1.php
@@ -3,13 +3,11 @@
 $string = "Hello, world!";
 $length = 0;
 
-// Iterate through each character of the string
 for ($i = 0; isset($string[$i]); $i++) {
     // Increment the length counter
     $length++;
 }
 
-// Output the length of the string
 echo "The length of the string is: " . $length;
 
 ?>
